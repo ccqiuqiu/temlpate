10f25bd88c797d559cd6c9894f78c2d6bb8638d7
cc <95353088@qq.com>
2021-08-23 16:24:18 +0800

    ✨ 修改一些配置和样式

--- src/assets/utils/utils.js
+++ src/assets/utils/utils.js
@@ -64,17 +64,17 @@ export default {
     }
     return retstr.replace(/^,+/, '').replace(/\.$/, '')
   },
-  formatDate (date, fmt = 'yyy-MM-dd') {
+  formatDate (date = new Date(), fmt = 'yyyy-MM-dd') {
     const o = {
-      'M+': this.getMonth() + 1, // 月份
-      'd+': this.getDate(), // 日
-      'h+': this.getHours(), // 小时
-      'm+': this.getMinutes(), // 分
-      's+': this.getSeconds(), // 秒
-      'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+      'M+': date.getMonth() + 1, // 月份
+      'd+': date.getDate(), // 日
+      'h+': date.getHours(), // 小时
+      'm+': date.getMinutes(), // 分
+      's+': date.getSeconds(), // 秒
+      'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
       S: this.getMilliseconds() // 毫秒
     }
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
     for (const k in o) {
       if (new RegExp('(' + k + ')').test(fmt)) {
         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
