2b7d56e0ea4befcade351d44c246e4f563bad584
cc <95353088@qq.com>
2021-09-06 11:37:40 +0800

    ✨

--- vue.config.js
+++ vue.config.js
@@ -49,19 +49,24 @@ module.exports = {
     if (process.env.NODE_ENV === 'production' || process.env.NO_MINIMIZE) {
       config.optimization.minimize(false)
     }
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule.test(/\.svg$/)
+    config.module.rule('svg')
+      .test(/\.svg$/)
+      .exclude
+      .add(resolve('src/assets/icons/svg'))
+      .end()
+    config.module.rule('svg-sprite')
+      .test(/\.(svg)(\?.*)?$/)
       .include
       .add(resolve('src/assets/icons/svg'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
-        symbolId: 'icon-[name]'
+        symbolId: 'icon-[name]',
       })
     // 设置一些别名
-    // config.resolve.alias
+    config.resolve.alias
+      .set('@', resolve('./src'))
     //   .set('@g', resolve('examples/global'))
     //   .set('@bc', resolve('examples/baseComponent'))
     //   .set('@common', resolve('examples/modules/common/view'))
