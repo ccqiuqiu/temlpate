ceaf44b7a00c72c72caeadfdb41410f79030d5fb
xiongchaochao <95353088@qq.com>
2022-08-16 09:42:58 +0800

    ✨ 菜单自定义

--- src/main.js
+++ src/main.js
@@ -14,13 +14,13 @@ import utils from './assets/utils'
 import './assets/icons/icon'
 import Logo from './views/Logo'
 // 国际化
-import messages from './locales'
+// import messages from './locales'
 
 Vue.config.productionTip = false
 
 Vue.use(qianxun, {
   appId: 'qianxun',
-  i18n: {messages},
+  // i18n: {messages},
   ui: {
     size: 'small',
     // forceMultiTab: true // url严格模式，强制参数不同的url打开新标签,
@@ -33,6 +33,7 @@ Vue.use(qianxun, {
     modules, // vuex子模块
   },
   components: {
+    // menu: Logo,
     title: Logo, // 网站标题
     // nav: Logo, // 网站导航部分扩展
     // login: Logo, // 登录页面
